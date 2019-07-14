2ca1e6a6fdba61af53042392da8414eeab4f7d1c
Gail Parsloe <gail.parsloe.developer@gmail.com>
2019-07-14 08:45:46 -0400

    initial

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PersonsComponent } from './persons/persons.component';
+import { AnimalsComponent } from './animals/animals.component';
+import { PersonInputComponent } from './persons/person-input.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PersonsComponent,
+    AnimalsComponent,
+    PersonInputComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/persons/person-input.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-person-input',
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
+})
+
+export class PersonInputComponent {
+  onCreatePerson(personName: string) {
+console.log('onCreatePerson just called: ' + personName);
+
+  }
+}
